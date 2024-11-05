2c13b6a0300a95bbf62b3c6bc7fceb77438b22fe
Yesh Gowda <134262636+Yesh-Gowda@users.noreply.github.com>
2024-11-05 15:41:59 +0530

    Update Yesh.java

--- Yesh.java
+++ Yesh.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class Yesh {
     public static void main(String[] args) {
         // Printing "Hello, World!" to the console
         System.out.println("Hello, World!");
